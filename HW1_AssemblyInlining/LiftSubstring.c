1fc114bc388429b1892a01b1b57c97118f5f5720
Hari <harinarayananmohan@gmail.com>
2016-01-31 05:29:32 -0500

    adding LiftSubstring.c program

---
+++ HW1_AssemblyInlining/LiftSubstring.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+static inline char * sub_str(char * dest, char *src, int s_idx, int edix){
+printf ("hari");
+  return dest;
+}
+
+static inline char * asm_sub_str(char * dest, char *src, int s_idx, int edix){
+print ("asm hari");
+  return dest;
+}
+
+int main(int argc,char **argv) {
+  char s[]=**argv[1];
+  int s_indx=(int)**argv[2];
+  int e_idx=(int)**argv[3];
+  d1=sub_str(d,s,s_idx,e_idx);
+  d2=asm_sub_str(d,s,s_idx,e_idx);
+
+}

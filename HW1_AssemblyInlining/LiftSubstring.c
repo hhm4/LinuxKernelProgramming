84c29046327b487e15044a9afa016af21d3f0932
Hari <harinarayananmohan@gmail.com>
2016-01-31 19:38:11 -0500

    modifying c part

--- HW1_AssemblyInlining/LiftSubstring.c
+++ HW1_AssemblyInlining/LiftSubstring.c
@@ -1,21 +1,29 @@
 #include<stdio.h>
+#include<string.h>
 
-static inline char * sub_str(char * dest, char *src, int s_idx, int edix){
-printf ("hari");
-  return dest;
+static inline char *sub_str(char *dest, char *src, int s_idx, int edix){
+char a[strlen(src)];
+
+strncpy(dest,src+s_idx,edix-s_idx);
+return dest;
 }
 
-static inline char * asm_sub_str(char * dest, char *src, int s_idx, int edix){
-print ("asm hari");
+static inline char *asm_sub_str(char * dest, char *src, int s_idx, int edix){
 
   return dest;
 }
 
 int main(int argc,char **argv) {
-  char * s=malloc(strlen(argv[1])+1);
-  strcpy(s,argv[1]);
-  int s_indx=(int)argv[2];
-  int e_idx=(int)argv[3];
-  d1=sub_str(d,s,s_idx,e_idx);
-  d2=asm_sub_str(d,s,s_idx,e_idx);
+ char *d;
+ char *d1;
+ char *d2;
+ char *s=argv[1];
+ int len=strlen(s);
+ int s_idx=atoi(argv[2]);
+ int e_idx=atoi(argv[3]);
+char a[len];
+d=a;
+d1=sub_str(d,s,s_idx,e_idx);
+ printf("d1:%s",d1);
+ d2=asm_sub_str(d,s,s_idx,e_idx);
 }
